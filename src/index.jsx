4cf5da4d2d32f4e2a60c9ee7484e571f2bc8d92b
Mohammad Faur <mohammad.fa@live.com>
2021-12-14 16:42:18 +0200

    working on navbar

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import App from './App';
+import App from "./App";
+import "antd/dist/antd.css";
 
 ReactDOM.render(
   <BrowserRouter>
