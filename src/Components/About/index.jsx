ac13f141c09010e390fdc6a13ff3a268c2f1fbcf
Nur Awad <nurawad95@gmail.com>
2021-12-14 11:04:33 +0200

    test stuff with mario

---
+++ src/Components/About/index.jsx
@@ -0,0 +1,8 @@
+import style from "./style.module.css"
+
+function About (props) {
+  return <h1 className={style.title}>About Page</h1>
+}
+
+
+export default About
---
+++ src/Components/Home/index.jsx
@@ -0,0 +1,7 @@
+import style from "./style.module.css"
+
+function Home (props) {
+  return <h1 className={style.title}>HomePage</h1>
+}
+
+export default Home
