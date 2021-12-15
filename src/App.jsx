80e45f5dffb0875c7bb79e2366d1a099eab8f6b7
deana17 <91081633+deana17@users.noreply.github.com>
2021-12-15 16:01:17 +0200

    items

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Home from "./Components/Home";
 import About from "./Components/About";
-
 import NavBar from "./Components/NavBar";
+import Dairy from "./Components/Store/Dairy";
+// import Item from "./Components/Item";
 
 function App() {
   return (
     <div className="App">
       <NavBar buttonValue="Logout" text="Welcome, Nur!" count="5" />
-
+      <Dairy />
+      {/* <Item imgUrl="/store/dairy/milk.jpg" name="Milk" price="10" /> */}
       <h1>Welcome to React Router!</h1>
       <Routes>
         <Route path="/" element={<Home />} />
---
+++ src/Products/dairyItems.js
@@ -0,0 +1,28 @@
+const dairyItems = [
+  {
+    id: "1",
+    imgUrl: "/store/dairy/1.jpg",
+    name: "Sugar",
+    price: 8,
+  },
+  {
+    id: "2",
+    imgUrl: "/store/dairy/2.jpg",
+    name: "Milk",
+    price: 10,
+  },
+  {
+    id: "3",
+    imgUrl: "/store/dairy/3.jpg",
+    name: "Salt",
+    price: 5,
+  },
+  {
+    id: "4",
+    imgUrl: "/store/dairy/4.jpg",
+    name: "Eggs",
+    price: 30,
+  },
+];
+
+export default dairyItems;
