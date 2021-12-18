3ece9072b121b9b9c78a81baf72a91d2ddc5ac90
Nur1995 <79867892+Nur1995@users.noreply.github.com>
2021-12-19 00:47:14 +0200

    reverted function to ids and changed item ids

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,4 +1,4 @@
 //given an array of objects and an object, then return the place of object in the array.
 export const findIdx = (arrayOfObjs, objInTheArray) => {
-  return arrayOfObjs.findIndex((item) => item.name === objInTheArray.name);
+  return arrayOfObjs.findIndex((item) => item.id === objInTheArray.id);
 };
