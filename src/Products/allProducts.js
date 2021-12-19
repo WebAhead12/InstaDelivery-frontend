71310ffd061ef279d3cdcd2cfe4e179bf8a662bf
Nur1995 <79867892+Nur1995@users.noreply.github.com>
2021-12-19 10:01:23 +0200

    added all products js for search autocomplete

--- src/Products/allProducts.js
+++ src/Products/allProducts.js
@@ -1,95 +1,150 @@
-const dairyItems = [
+const allProducts = [
   {
-    id: "1",
+    id: "101",
     imgUrl: "/store/dairy/1 milk.jpg",
     name: "1% Milk",
     price: "10.90",
   },
   {
-    id: "2",
+    id: "102",
     imgUrl: "/store/dairy/3 milk.jpg",
     name: "3% Milk",
     price: "10.90",
   },
   {
-    id: "3",
+    id: "103",
     imgUrl: "/store/dairy/large yogurt.jpg",
     name: "Large Yogurt",
     price: "25.90",
   },
   {
-    id: "4",
+    id: "104",
     imgUrl: "/store/dairy/eggs.jpg",
     name: "Eggs",
     price: "29.90",
   },
 
   {
-    id: "5",
+    id: "105",
     imgUrl: "/store/dairy/oat milk.jpg",
     name: "Oat Milk",
     price: "11.90",
   },
   {
-    id: "6",
+    id: "106",
     imgUrl: "/store/dairy/almond milk.jpg",
     name: "Almond Milk",
     price: "11.90",
   },
   {
-    id: "7",
+    id: "107",
     imgUrl: "/store/dairy/hazelnut milk.jpg",
     name: "Hazelnut Milk",
     price: "11.90",
   },
   {
-    id: "8",
+    id: "108",
     imgUrl: "/store/dairy/cashew milk.jpg",
     name: "Cashew Milk",
     price: "11.90",
   },
   {
-    id: "9",
+    id: "109",
     imgUrl: "/store/dairy/soy milk.jpg",
     name: "Soy Milk",
     price: "11.90",
   },
   {
-    id: "10",
+    id: "1010",
     imgUrl: "/store/dairy/coconut milk.jpg",
     name: "Coconut Milk",
     price: "11.90",
   },
   {
-    id: "11",
+    id: "1011",
     imgUrl: "/store/dairy/chocolate soy milk.jpg",
     name: "Choc. Soy Milk",
     price: "11.90",
   },
   {
-    id: "12",
+    id: "1012",
     imgUrl: "/store/dairy/vanilla soy milk.jpg",
     name: "Van. Soy Milk",
     price: "11.90",
   },
   {
-    id: "13",
+    id: "1013",
     imgUrl: "/store/dairy/yellow cheese.jpg",
     name: "Yellow Cheese",
     price: ".90",
   },
   {
-    id: "14",
+    id: "1014",
     imgUrl: "/store/dairy/mozzerella cheese.jpg",
     name: "Mozzerella",
     price: "19.90",
   },
   {
-    id: "15",
+    id: "1015",
     imgUrl: "/store/dairy/cheddar.jpg",
     name: "Cheddar",
     price: "23.90",
   },
+
+  {
+    id: "201",
+    imgUrl: "/store/bakery/bagels.jpg",
+    name: "Bagels 1c.",
+    price: "1.90",
+  },
+  {
+    id: "202",
+    imgUrl: "/store/bakery/baguette.jpg",
+    name: "Baguettes 2c.",
+    price: "2.90",
+  },
+  {
+    id: "203",
+    imgUrl: "/store/bakery/bread no gluten.jpg",
+    name: "Gluten-free loaf",
+    price: "12.90",
+  },
+  {
+    id: "204",
+    imgUrl: "/store/bakery/half baguettes.jpg",
+    name: "Half-baguette 7c.",
+    price: "4.90",
+  },
+  {
+    id: "205",
+    imgUrl: "/store/bakery/burger buns.png",
+    name: "Burger Buns 4c.",
+    price: "9.90",
+  },
+  {
+    id: "206",
+    imgUrl: "/store/bakery/wheat bread.jpg",
+    name: "Wheat Loaf",
+    price: "10.90",
+  },
+  {
+    id: "207",
+    imgUrl: "/store/bakery/white bread.jpg",
+    name: "White Loaf",
+    price: "10.90",
+  },
+  {
+    id: "208",
+    imgUrl: "/store/bakery/croissant.jpg",
+    name: "Croissant 1c.",
+    price: "1.90",
+  },
+  {
+    id: "209",
+    imgUrl: "/store/bakery/chocolate croissant.jpg",
+    name: "Choc. Croissant",
+    price: "1.90",
+  },
 ];
 
-export default dairyItems;
+export default allProducts;
